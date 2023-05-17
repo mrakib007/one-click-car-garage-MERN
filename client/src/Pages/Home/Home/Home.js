2b80a1b2589c0bdaa552cebe2fb354cb322d3009
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-05-17 22:51:17 +0600

    Added new sectoins and cleaned the code

--- client/src/Pages/Home/Home/Home.js
+++ client/src/Pages/Home/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Service from '../Service/Service';
+import WhyUs from '../WhyUs/WhyUs';
 
 const Home = () => {
     return (
         <div className='my-2'>
             <Banner/>
+            <WhyUs/>
             <Service/>
         </div>
     );
