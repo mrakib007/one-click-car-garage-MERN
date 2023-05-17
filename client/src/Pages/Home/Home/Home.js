869af4078b208d5668a56928f125fe0385204c59
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-05-17 11:51:22 +0600

    added hero component and navbar

---
+++ client/src/Pages/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+
+const Home = () => {
+    return (
+        <div className='my-2'>
+            <Banner/>
+        </div>
+    );
+};
+
+export default Home;
