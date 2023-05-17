17f8bff40db68797cebd2aa56dd6fbff294a4b78
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-05-17 20:14:43 +0600

    Added cards for services info

--- client/src/Pages/Home/Home/Home.js
+++ client/src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Service from '../Service/Service';
 
 const Home = () => {
     return (
         <div className='my-2'>
             <Banner/>
+            <Service/>
         </div>
     );
 };
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,18 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./src/**/*.{html,js}"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [require("daisyui")],
+// }
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
-}
-
+});
