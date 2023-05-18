f8f4fa09617f4c292502dfee3aed64ede0fa0a0d
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-05-18 15:16:21 +0600

    Added photo gallery

--- client/src/Pages/Home/Home/Home.js
+++ client/src/Pages/Home/Home/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import Service from '../Service/Service';
 import WhyUs from '../WhyUs/WhyUs';
+import PhotoGallery from '../PhotoGallery/PhotoGallery';
 
 const Home = () => {
     return (
         <div className='my-2'>
             <Banner/>
             <WhyUs/>
+            <PhotoGallery/>
             <Service/>
         </div>
     );
---
+++ client/src/Pages/Home/PhotoGallery/PhotoGallery.js
@@ -0,0 +1,104 @@
+import React from 'react';
+
+const PhotoGallery = () => {
+    return (
+        
+// {/* <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+//     <div class="grid gap-4">
+//         <div>
+//             <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt=""/>
+//         </div>
+//         <div>
+//             <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt=""/>
+//         </div>
+//         <div>
+//             <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt=""/>
+//         </div>
+//     </div>
+//     <div class="grid gap-4">
+//         <div>
+//             <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt=""/>
+//         </div>
+//         <div>
+//             <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt=""/>
+//         </div>
+//         <div>
+//             <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt=""/>
+//         </div>
+//     </div>
+//     <div class="grid gap-4">
+//         <div>
+//             <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt=""/>
+//         </div>
+//         <div>
+//             <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt=""/>
+//         </div>
+//         <div>
+//             <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt=""/>
+//         </div>
+//     </div>
+//     <div class="grid gap-4">
+//         <div>
+//             <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt=""/>
+//         </div>
+//         <div>
+//             <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt=""/>
+//         </div>
+//         <div>
+//             <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt=""/>
+//         </div>
+//     </div>
+// </div>  */}
+
+<div class="mx-auto px-5 py-2 lg:px-32 lg:pt-24">
+<h1 class="max-w-2xl my-10 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-4xl mx-auto dark:text-white">Make No Compromise With Quality</h1>
+  <div class="-m-1 flex flex-wrap md:-m-2">
+    <div class="flex w-1/2 flex-wrap">
+      <div class="w-1/2 p-1 md:p-2">
+        <img
+          alt="gallery"
+          class="block h-full w-full rounded-lg object-cover object-center"
+          src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp" />
+      </div>
+      <div class="w-1/2 p-1 md:p-2">
+        <img
+          alt="gallery"
+          class="block h-full w-full rounded-lg object-cover object-center"
+          src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp" />
+      </div>
+      <div class="w-full p-1 md:p-2">
+        <img
+          alt="gallery"
+          class="block h-full w-full rounded-lg object-cover object-center"
+          src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" />
+      </div>
+    </div>
+    <div class="flex w-1/2 flex-wrap">
+      <div class="w-full p-1 md:p-2">
+        <img
+          alt="gallery"
+          class="block h-full w-full rounded-lg object-cover object-center"
+          src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp" />
+      </div>
+      <div class="w-1/2 p-1 md:p-2">
+        <img
+          alt="gallery"
+          class="block h-full w-full rounded-lg object-cover object-center"
+          src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp" />
+      </div>
+      <div class="w-1/2 p-1 md:p-2">
+        <img
+          alt="gallery"
+          class="block h-full w-full rounded-lg object-cover object-center"
+          src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(77).webp" />
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+    );
+};
+
+export default PhotoGallery;
