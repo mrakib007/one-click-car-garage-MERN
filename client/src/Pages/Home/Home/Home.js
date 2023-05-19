4038136d132843153d9f318bf4c0dbdaa30eeb01
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-05-19 11:31:56 +0600

    Added testimonial section

--- client/src/Pages/Home/Home/Home.js
+++ client/src/Pages/Home/Home/Home.js
@@ -3,14 +3,16 @@ import Banner from '../Banner/Banner';
 import Service from '../Service/Service';
 import WhyUs from '../WhyUs/WhyUs';
 import PhotoGallery from '../PhotoGallery/PhotoGallery';
+import Testimonial from '../Testimonia/Testimonial';
 
 const Home = () => {
     return (
         <div className='my-2'>
             <Banner/>
             <WhyUs/>
-            <PhotoGallery/>
             <Service/>
+            <PhotoGallery/>
+            <Testimonial/>
         </div>
     );
 };
