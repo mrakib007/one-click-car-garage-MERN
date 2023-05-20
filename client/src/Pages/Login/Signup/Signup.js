45ec2f718277abd07cd723581df29dcab681d42c
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-05-20 12:43:22 +0600

    Adding firebase signup functionalitis

--- client/src/Pages/Login/Signup/Signup.js
+++ client/src/Pages/Login/Signup/Signup.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthProvider';
+import { GoogleAuthProvider } from 'firebase/auth';
 
 const Signup = () => {
+  const [name,setName] = useState('');
+  const [email,setEmail] = useState('');
+  const [password,setPassword] = useState('');
+  const [signupError,setSignupError] = useState('');
+  const googleProvider = new GoogleAuthProvider();
+  const {createUser,updateUser} = useContext(AuthContext);
+
+  const handleSignUp = (event) =>{
+    event.preventDefault();
+    setSignupError('');
+    createUser(email,password)
+    .then(result =>{
+      const user = result.user;
+      console.log(user);
+      const userInfo = {
+        displayName: name
+      }
+      updateUser(userInfo);
+    })
+    .then({})
+    .catch(error => {
+      setSignupError(error.message);
+    })
+  }
     return (
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -11,12 +37,30 @@ const Signup = () => {
             alt="Your Company"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
+            Sign up to register.
           </h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
+          <form onSubmit={handleSignUp} className="space-y-6" action="#" method="POST">
+            <div>
+              <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+                Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  autoComplete="name"
+                  value={name}
+                  onChange={event => setName(event.target.value)}
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                 Email address
@@ -28,6 +72,8 @@ const Signup = () => {
                   type="email"
                   autoComplete="email"
                   required
+                  value={email}
+                  onChange={event => setEmail(event.target.value)}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -51,6 +97,8 @@ const Signup = () => {
                   type="password"
                   autoComplete="current-password"
                   required
+                  value={password}
+                  onChange={event => setPassword(event.target.value)}
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
@@ -64,6 +112,7 @@ const Signup = () => {
                 Sign in
               </button>
             </div>
+            {signupError && <p className='text-red-500'>{signupError}</p>}
           </form>
 
           <p className="mt-10 text-center text-sm text-gray-500">
--- client/src/contexts/AuthProvider.js
+++ client/src/contexts/AuthProvider.js
@@ -1,18 +1,31 @@
-import React, { createContext } from 'react';
-import app from '../firebase/firebase.config';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import React, { createContext, useState } from "react";
+import app from "../firebase/firebase.config";
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-const authInfo = {
-    
-}
-const AuthProvider = ({children}) => {
-   return <AuthContext.Provider value={authInfo}>
-    {children}
-   </AuthContext.Provider>
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const updateUser = (userInfo) =>{
+    return updateProfile(auth.currentUser,userInfo);
+  }
+  const authInfo = {
+    user,
+    createUser,
+    updateUser,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>
+        {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
