9026c972d7a19e990f04f2ffde2c6ad72f74662e
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-05-25 11:23:28 +0600

    adding end point for admin

--- server/index.js
+++ server/index.js
@@ -79,6 +79,14 @@ async function run() {
       const users = await usersCollection.find(query).toArray();
       res.send(users);
     });
+
+    app.get('/users/admin/:email',async(req,res)=>{
+      const email = req.params.email;
+      const query = {email};
+      const user = await usersCollection.findOne(query);
+      res.send({isAdmin: user?.roe === 'admin'});
+    })
+    
   } finally {
   }
 }
