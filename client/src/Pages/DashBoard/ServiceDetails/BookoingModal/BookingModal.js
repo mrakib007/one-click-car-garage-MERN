2f94fe56d85751fdf163568be1f82082b16397e5
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-06-03 16:17:22 +0600

    Added backend code for bookings

--- client/src/Pages/DashBoard/ServiceDetails/BookoingModal/BookingModal.js
+++ client/src/Pages/DashBoard/ServiceDetails/BookoingModal/BookingModal.js
@@ -1,14 +1,75 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../../../../contexts/AuthProvider";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const BookingModal = ({serviceBooking,setServiceBooking}) => {
     const {user} = useContext(AuthContext);
     const { serviceName, description, price, image,name } = serviceBooking;
     const handleBooking = (event) =>{
         event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const price = form.price.value;
+        const phone = form.phone.value;
+
+        const booking = {
+          name: name,
+          serviceBooking: serviceName,
+          image_url: image,
+          email,
+          price,
+          phone,
+        }
+        fetch('http://localhost:5000/bookings',{
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json'
+          },
+          body: JSON.stringify(booking)
+        }).then(res => res.json())
+        .then(data => {
+          if(data.acknowledged){
+            setServiceBooking(null);
+            toast.success("Booking Successful!", {
+              position: "top-left",
+              autoClose: 2000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "dark",
+            });
+          }else{
+            toast.error("Error Happened!", {
+              position: "top-left",
+              autoClose: 2000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "dark",
+            });
+          }
+        })
     }
     return (
     <>
+    <ToastContainer
+        position="bottom-left"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
       <input type="checkbox" id="booking-modal" className="modal-toggle" />
       <div className="modal">
         <div className="modal-box relative">
