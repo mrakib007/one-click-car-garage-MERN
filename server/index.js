ab004cd93708a5d97c46dd0a783791ef7ab31e30
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-06-04 10:51:41 +0600

    Adding stripe payment routes backend

--- server/index.js
+++ server/index.js
@@ -171,6 +171,38 @@ async function run() {
       const booking = await bookingsCollection.findOne(query);
       res.send(booking);
     })
+
+    //Payment related routes
+    app.post('/create-payment-intent',async(req,res)=>{
+      const booking = req.body;
+      const price = booking.price;
+      const amount = price * 100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        currency: 'bdt',
+        amount: amount,
+        "payment_method_types": [
+          "card"
+        ]
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      })
+    })
+
+    app.post('/payments',async(req,res)=>{
+      const payment = req.body;
+      const result = await paymentsCollection.insertOne(payment);
+      const id = payment.bookingId;
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          paid: true,
+          transactionId: payment.transactionId,
+        }
+      }
+      const updatedResult = await bookingsCollection.updateOne(filter,updatedDoc);
+      res.send(result);
+    })
     
   } finally {
   }
