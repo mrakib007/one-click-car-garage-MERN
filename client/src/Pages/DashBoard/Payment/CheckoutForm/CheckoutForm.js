43f7d49f223f756f8d051b9864079018374bdd52
Rakib Hasan <mrakibhasan199744@gmail.com>
2023-06-04 19:19:19 +0600

    Adding checkout form

--- client/src/Pages/DashBoard/Payment/CheckoutForm/CheckoutForm.js
+++ client/src/Pages/DashBoard/Payment/CheckoutForm/CheckoutForm.js
@@ -1,6 +1,31 @@
-import React from 'react';
+import { Elements, useElements, useStripe } from '@stripe/react-stripe-js';
+import React, { useEffect, useState } from 'react';
 
-const CheckoutForm = () => {
+const CheckoutForm = ({booking}) => {
+    console.log(booking)
+    const {price,email,name,_id} = booking;
+    const stripe = useStripe();
+    const elements = useElements();
+    const [clientSecret,setClientSecret] = useState('');
+    useEffect(() =>{
+        fetch('http://localhost:5000/create-payment-intent',{
+            method: 'POST',
+            headers:  {
+                "Content-Type": "application/json",
+                // authorization: `bearer ${localStorage.getItem("accessToken")}`
+            },
+            body: JSON.stringify({price}),
+        })
+        .then((res) => res.json())
+        .then((data)=> console.log(data.clientSecret));
+    },[price])
+
+    const handleSubmit = async(event) =>{
+        event.preventDefault();
+        if(!stripe || !elements){
+            return;
+        }
+    }
     return (
         <div>
             
